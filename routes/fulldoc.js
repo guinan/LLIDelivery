73251ef2ec2a109a67c9dd1df3ecc20090d19866
geekmom79 <github@geek-mom.de>
2016-04-16 17:02:34 +0200

    added fulldoc retrieval

---
+++ routes/fulldoc.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+const Browser = require('zombie');
+const browser = new Browser();
+
+//Browser.localhost('http://vishub.org/excursions/2209', 3000)
+
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	// //console.log("Yep");
+	// //putting triples into the DB
+ //  //var lli = llidata.ex1;
+   console.log("loading: "+req.query.res_uri);
+ //  //putting data into database
+   browser.visit(req.query.res_uri, function() {
+   //console.log(browser.location.href);
+   res.status(200).send(browser.document.documentElement.innerHTML);
+ //  if(browser.)
+  
+     if(browser.html()){
+       res.status(200).send(browser.html());
+     
+       } ;
+  
+ //  //console.log(browser.document.documentElement.innerHTML);
+ //  });
+ //   browser.close(); 
+
+ //   browser.visit(req.query.res_uri)
+  //        .then(function() {res.status(200).send(browser.html())});
+});
+ });
+
+module.exports = router;
